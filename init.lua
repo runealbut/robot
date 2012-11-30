b8addc523c0626b258461f9579718cd611020964
runealbut <etwas@runealbut.de>
2012-11-30 21:39:44 +0100

    liecense  commit

--- init.lua
+++ init.lua
@@ -1,6 +1,10 @@
 
 
 --[[
+Copyright (C) 2012 runealbut,Tobias ...  <etwas@runealbut.de>
+
+https://github.com/runealbut/robot.git
+
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
